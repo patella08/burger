afa947f55459d8984d23f9d4f9126b0ccc980bd0
Anisha Patel <patella08@yahoo.com>
2018-01-20 00:01:18 -0500

    Final Changes

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured, date) VALUES ("Fish Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ("Chicken Ranch Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ("Hamburger", FALSE);
